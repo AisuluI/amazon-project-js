a48a35a28dc14d0411b42eda7e6d46f83d696343
Aisulu <aisulu.imankulova@gmail.com>
2024-09-12 13:12:24 -0400

    import cart and products from data

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,7 @@
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
+
+
 // const products = [
 //   {
 //     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
